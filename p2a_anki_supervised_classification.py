2f569b94d597ea817db78a9e33e3685ba887894c
NicolasMICAUX <micaux.nicolas@gmail.com>
2023-01-12 17:05:59 +0100

    use bs=32

--- p2a_anki_supervised_classification.py
+++ p2a_anki_supervised_classification.py
@@ -38,7 +38,7 @@ tokenized_dataset = load_dataset("nicolasmicaux/anki_data", use_auth_token=True)
 
 task = "chunk" # Should be one of "ner", "pos" or "chunk" : je pense chunk marchera le mieux si je pars d'un truc pretrained
 model_checkpoint = 'xlm-roberta-base'
-batch_size = 8
+batch_size = 32
 
 # Use GPU 1
 import os
